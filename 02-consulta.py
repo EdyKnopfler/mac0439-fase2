d9f5951f6d9612c281366afc89de610a6f0caeac
kodaira <naomicamila@hotmail.com>
2018-05-19 23:12:11 -0300

    Mudei id do auncio consultado do arquicvo '02-consulta.py'

--- 02-consulta.py
+++ 02-consulta.py
@@ -3,11 +3,12 @@ import psycopg2
 opcoes = "dbname='' user='' host='linux.ime.usp.br' password='' port='5432'" #Altere essa linha! - Para alterar o DB que sera acessado
 conexao = psycopg2.connect(opcoes)
 cursor = conexao.cursor()
+cursor.execute("SET search_path TO mac0439") #Altere essa linha - Para mudar o Schema utilizado
 
 cursor.execute("""
    SELECT COUNT(*)
    FROM requisito
-   WHERE id_anuncio = 3 AND tipo = 'Obrigatorio'
+   WHERE id_anuncio = 1 AND tipo = 'Obrigatorio'
 """);
 
 quantidade = cursor.fetchone()[0]
@@ -21,7 +22,7 @@ cursor.execute("""
       JOIN requisito req
          ON st.id_anuncio = req.id_anuncio AND st.titulo = req.titulo
       JOIN usuario u ON st.email_candidato = u.email
-   WHERE st.id_anuncio = 3 AND st.status = 'cumprido' AND req.tipo = 'Obrigatorio'
+   WHERE st.id_anuncio = 1 AND st.status = 'cumprido' AND req.tipo = 'Obrigatorio'
    GROUP BY u.nome
 """)
 
@@ -36,7 +37,7 @@ cursor.execute("""
       JOIN requisito req
          ON st.id_anuncio = req.id_anuncio AND st.titulo = req.titulo
       JOIN usuario u ON st.email_candidato = u.email
-   WHERE st.id_anuncio = 3 AND st.status = 'cumprido' AND req.tipo = 'Opcional'
+   WHERE st.id_anuncio = 1 AND st.status = 'cumprido' AND req.tipo = 'Opcional'
    GROUP BY u.nome
 """)
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # mac0439-fase2
 Fase 2 do projeto da disciplina Laboratório de Bancos de Dados
 
-Leia a parte 5 do documento "MAC0439 Fase 1 - Requisitos funcionais e de dados e modelagem conceitual" para instruções
+Leia a parte 5 do documento "MAC0439 Fase 1 - Requisitos funcionais e de dados e modelagem conceitual" para instruções de como rodar os scripts
